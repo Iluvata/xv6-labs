99b91356881d907a3d3f444782f4bb8fccd7278b
iluvata <1124026683@qq.com>
2023-08-30 10:16:19 +0800

    symlink

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -169,6 +169,68 @@ bad:
   return -1;
 }
 
+uint64
+sys_symlink(void)
+{
+  char name[DIRSIZ], new[MAXPATH], old[MAXPATH];
+  struct inode *dp, *ip;
+
+  if(argstr(0, old, MAXPATH) < 0 || argstr(1, new, MAXPATH) < 0)
+    return -1;
+
+  // begin_op();
+  // if((ip = namei(old)) == 0){
+  //   end_op();
+  //   return -1;
+  // }
+
+  // ilock(ip);
+  // if(ip->type == T_DIR){
+  //   iunlockput(ip);
+  //   end_op();
+  //   return -1;
+  // }
+
+  // ip->nlink++;
+  // iupdate(ip);
+  // iunlock(ip);
+  begin_op();
+  ip = create(new, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, old, 0, MAXPATH) != MAXPATH){
+    ip->nlink--;
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlock(ip);
+
+  if((dp = nameiparent(new, name)) == 0)
+    goto bad;
+  ilock(dp);
+  if(dirlink(dp, name, ip->inum) < 0){
+    iunlockput(dp);
+    goto bad;
+  }
+  iunlockput(dp);
+  iput(ip);
+
+  end_op();
+
+  return 0;
+
+bad:
+  ilock(ip);
+  ip->nlink--;
+  iupdate(ip);
+  iunlockput(ip);
+  end_op();
+  return -1;
+}
+
 // Is the directory dp empty except for "." and ".." ?
 static int
 isdirempty(struct inode *dp)
